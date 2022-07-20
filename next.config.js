11e38a0fbf3dc8d18c71da68af419d67f428c262
Thibault Feraux <thibault.feraux@hotmail.com>
2022-07-20 11:39:33 +0200

    :adhesive_bandage: Fixed build error for eslint

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    module.exports = {
-        typescript: {
-            ignoreBuildErrors: true,
-        },
-    }
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
